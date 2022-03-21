b5d1b2907a5c9899127cad0eaf622590327a92bc
Gokulnath99 <gokulnathanand23@gmail.com>
2022-03-21 20:03:19 +0530

    added new and improved carosel to the blog-project

--- pages/index.js
+++ pages/index.js
@@ -5,26 +5,23 @@ import { getPosts } from '../services';
 export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-12 mb-8 mt-20">
-      <div className="">
-        <div>
-          <Carousel/>
-        </div>
-        
-        
         {/* <FeaturedPosts /> */}
-        <div className="grid grid-cols-1 mt-6 lg:grid-cols-12 gap-12">
-          <div className="lg:col-span-8 col-span-1">
-            {posts.map((post, index) => (
-              <PostCard key={index} post={post.node} />
-            ))}
-          </div>
-          <div className="lg:col-span-4 col-span-1">
-            <div className="lg:sticky relative top-8">
-              <PostWidget />
-              <Categories />
-            </div>
+      <div className="grid grid-cols-1 mt-6 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          <Carousel />
+        </div>
+        <div className="lg:col-span-4 col-span-1 row-span-2">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
           </div>
         </div>
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+        
       </div>
     </div>
       
